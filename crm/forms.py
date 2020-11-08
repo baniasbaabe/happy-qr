c7b5e641ecee023a78a5430040de2d4b91128b34
EasymanPGM <lennart.unruh@web.de>
2020-11-08 17:19:56 +0100

    Mitarbeiter CRUD vollständig erstellt, einige Button Texte angepasst, Button Namenskonvention angepasst, Button Icons eingefügt, Crispy-Forms installiert und in Requirements ergänzt, Base.html wurde um Mitarbeiter ergänzt

--- crm/forms.py
+++ crm/forms.py
@@ -1,7 +1,15 @@
 from django.forms import ModelForm
 from .models import *
 
+
 class KundeForm(ModelForm):
     class Meta:
         model = Kunde
         fields = '__all__'
+
+
+class MitarbeiterForm(ModelForm):
+    class Meta:
+        model = Mitarbeiter
+        fields = '__all__'
+
