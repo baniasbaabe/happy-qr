7d32982ba68c0df5487324ab31dadfc8f4fd4762
baniasbaabe <banias@hotmail.de>
2020-11-29 15:29:35 +0100

    Headless Mode aktivieren

--- menucard/tests/test_kunde_seite.py
+++ menucard/tests/test_kunde_seite.py
@@ -10,9 +10,9 @@ class TestKundeSeite(StaticLiveServerTestCase):
     @classmethod
     def setUp(cls):
         chrome_options = webdriver.ChromeOptions()
-        #chrome_options.add_argument('--no-sandbox')
-        #chrome_options.add_argument('--headless')
-        #chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
         cls.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
 
     @classmethod
