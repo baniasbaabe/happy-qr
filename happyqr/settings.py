5a664399c243a04e9604cd24af31e345b93c56f0
baniasbaabe <banias@hotmail.de>
2020-12-27 16:29:19 +0100

    Static Folder mit collectstatic erstellt

--- happyqr/settings.py
+++ happyqr/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #or staticfiles
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
