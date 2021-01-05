222dfa34f71522f602781ff159b576ee497fb6ad
Fidel <fidel.synmz@gmail.com>
2021-01-05 17:34:19 +0100

    Selenium-Test angepasst

--- menucard/tests/test_selenium.py
+++ menucard/tests/test_selenium.py
@@ -14,9 +14,9 @@ class TestKundeSeite(LiveServerTestCase):
     @classmethod
     def setUp(cls):
         chrome_options = webdriver.ChromeOptions()
-       # chrome_options.add_argument('--no-sandbox')
-        #chrome_options.add_argument('--headless')
-        #chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--disable-gpu')
         cls.client = Client()
         cls.browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
         cls.user = User.objects.create_superuser(username="hallo123", password="dasisteintest123!")
